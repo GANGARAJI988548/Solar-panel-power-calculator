784bd13382089679b3d864fa5a8275563d16df92
GANGARAPU RAJESWARI <252ta02009@ashokacollege.in>
2026-08-23 11:14:35 +0530

    Create solar_panel_power.py

---
+++ solar_panel_power.py
@@ -0,0 +1,8 @@
+print("Solar Panel Power Calculator")
+
+voltage = float(input("Enter solar panel voltage (V): "))
+current = float(input("Enter solar panel current (A): "))
+
+power = voltage * current
+
+print("Solar Panel Power =", power, "W")
